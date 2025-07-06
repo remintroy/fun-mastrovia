dd3c29fd75efa8df454801a4ccc9da54aece8e61
Remin T Roy <86014784+remintroy@users.noreply.github.com>
2025-07-06 19:54:44 +0530

    feat(fifteen-puzzle): implement game logic and tile movement functionality

--- app/(fifteen-puzzle)/page.tsx
+++ app/(fifteen-puzzle)/page.tsx
@@ -1,6 +1,72 @@
+"use client";
+
+import { cn } from "@/lib/utils";
 import { VibrateIcon, Volume2Icon } from "lucide-react";
+import { useState } from "react";
+
+const generateCleanMatrix = (size = 4) => {
+  const matrix: number[][] = [];
+  let count = 1;
+
+  for (let i = 0; i < size; i++) {
+    matrix[i] = [];
+    for (let j = 0; j < size; j++) {
+      matrix[i][j] = i == size - 1 && j == size - 1 ? -1 : count++;
+    }
+  }
+
+  return matrix;
+};
+
+const findEmptyTile = (matrix: number[][]) => {
+  if (!matrix || !matrix.length) return null;
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === -1) {
+        return { x: i, y: j };
+      }
+    }
+  }
+
+  return null;
+};
+
+const isValidMove = (matrix: number[][], x: number, y: number) => {
+  if (!matrix || !matrix.length) return false;
+
+  const emptyTile = findEmptyTile(matrix);
+  if (!emptyTile) return false;
+
+  return (Math.abs(x - emptyTile.x) === 1 && y === emptyTile.y) || (Math.abs(y - emptyTile.y) === 1 && x === emptyTile.x);
+};
+
+const moveTile = (matrix: number[][], x: number, y: number) => {
+  if (!matrix || !matrix.length || !isValidMove(matrix, x, y)) return;
+
+  const emptyTile = findEmptyTile(matrix)!;
+  const newMatrix = matrix.map((row) => [...row]);
+
+  newMatrix[emptyTile.x][emptyTile.y] = matrix[x][y];
+  newMatrix[x][y] = -1;
+
+  return newMatrix;
+};
 
 export default function FifteenPuzzleHome() {
+  const [matrix, setMatrix] = useState<number[][]>(generateCleanMatrix());
+
+  const handleTileClick = (x: number, y: number) => {
+    const newMatrix = moveTile(matrix, x, y);
+    if (newMatrix) {
+      setMatrix(newMatrix);
+    }
+  };
+
+  const handleResetClick = () => {
+    setMatrix(generateCleanMatrix());
+  };
+
   return (
     <div className="w-full h-full flex flex-col gap-[10px] items-center justify-center">
       <div className="w-[350px] sm:w-[500px] h-[60px] flex justify-between gap-[10px]">
@@ -15,7 +81,7 @@ export default function FifteenPuzzleHome() {
           </div>
         </div>
         <div className="flex gap-[10px]">
-          <div className="border items-center justify-cente bg-black/30 backdrop-blur-xs w-min h-full p-2 px-4 flex gap-[20px]">
+          <div className="border items-center justify-center bg-black/30 backdrop-blur-xs w-min h-full p-2 px-4 flex gap-[20px]">
             <VibrateIcon />
           </div>
           <div className="border items-center justify-center bg-black/30 backdrop-blur-xs w-min h-full p-2 px-4 flex gap-[20px]">
@@ -24,21 +90,30 @@ export default function FifteenPuzzleHome() {
         </div>
       </div>
       <div className="w-[350px] h-[350px] sm:w-[500px] sm:h-[500px] gap-[10px] p-[15px] sm:p-[25px] box-border border bg-black/30 backdrop-blur-xs grid grid-cols-4 ">
-        {Array(15)
-          .fill(0)
-          .map((_, i) => {
+        {matrix.map((row, rowIndex) => {
+          return row?.map((item, colIndex) => {
             return (
               <div
-                key={i}
-                className="w-[72px] sm:w-[105px] h-[72px] sm:h-[105px] bg-[#fdffef] text-black flex items-center justify-center text-xl font-bold cursor-pointer"
+                key={item}
+                onClick={() => handleTileClick(rowIndex, colIndex)}
+                className={cn(
+                  "w-[72px] sm:w-[105px] h-[72px] sm:h-[105px] bg-[#fdffef] text-black flex items-center justify-center text-xl font-bold cursor-pointer",
+                  item == -1 ? "bg-accent" : ""
+                )}
               >
-                {i + 1}
+                {item == -1 ? "" : item}
               </div>
             );
-          })}
+          });
+        })}
         <div className="w-[72px] sm:w-[105px] h-[72px] sm:h-[105px] bg-accent"></div>
       </div>
-      <div className="w-[350px] sm:w-[500px] h-[60px] border bg-black/30 backdrop-blur-xs flex items-center justify-center">Restart</div>
+      <div
+        onClick={handleResetClick}
+        className="w-[350px] sm:w-[500px] h-[60px] border bg-black/30 backdrop-blur-xs flex items-center justify-center"
+      >
+        Restart
+      </div>
       {/* <div className="w-[350px] sm:w-[500px] h-[60px] flex items-center justify-center px-4">Move tiles in grid to order them from 1 to 15.</div> */}
     </div>
   );
