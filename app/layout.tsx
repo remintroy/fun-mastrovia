7b5667432055986bed444fb8e9968ef6e485c4a9
Remin T Roy <86014784+remintroy@users.noreply.github.com>
2025-07-11 18:39:23 +0530

    fix: update Google Tag Manager scripts to use environment variable for analytics ID

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,13 +31,13 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
-        <Script src="https://www.googletagmanager.com/gtag/js?id=G-PYXMK94XMQ" strategy="afterInteractive" />
+        <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_ID}`} strategy="afterInteractive" />
         <Script id="gtag-init" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-PYXMK94XMQ');
+            gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS_ID}', {);
           `}
         </Script>
       </body>
