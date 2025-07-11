d8c26678e1a65920dfaf02fcba2a8bb9b7351538
Remin T Roy <86014784+remintroy@users.noreply.github.com>
2025-07-11 19:04:11 +0530

    fix: correct syntax error in Google Tag Manager configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS_ID}', {);
+            gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS_ID}');
           `}
         </Script>
       </body>
